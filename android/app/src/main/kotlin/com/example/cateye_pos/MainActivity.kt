e65e3aabd0a8c931c1bc106cb665286e52267f27
Duc Le <ducle84@gmail.com>
2026-02-02 14:18:58 -0600

    Final project configuration updates

--- android/app/src/main/kotlin/com/example/cateye_pos/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cateye_pos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cateye_pos/main.dart';
+import 'package:bluestar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
